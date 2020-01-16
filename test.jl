d978267928e2e6d232a0ceed756c6677ac6378eb
bastikusch <bastikusch@live.com>
2020-01-16 21:49:30 +0100

    mutable sturct

--- test.jl
+++ test.jl
@@ -1,4 +1,12 @@
 # trying out some stuff as of now
 
 
-println("hello")
+mutable struct Episode{E<:AbstractEnvironment,P<:AbstractPolicy,F<:AbstractFloat}
+  env::E
+  policy::P
+  total_reward::F # total reward of the episode
+  last_reward::F
+  niter::Int      # current step in this episode
+  freq::Int       # number of steps between choosing actions
+  maxn::Int       # max steps in an episode - should be constant during an episode
+end
