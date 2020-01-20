4acc7c78e01e4cf2361af3ef741636af6b86b1dd
bastikusch <bastikusch@live.com>
2020-01-20 20:47:52 +0100

    layers

---
+++ layers/basic.jl
@@ -0,0 +1,50 @@
+
+struct Chain{T<:Tuple}
+  layers::T
+  Chain(xs...) = new{typeof(xs)}(xs)
+end
+
+
+function activations(c::Chain, input)
+    extraChain(c.layers, input)
+end
+
+function extraChain(fs::Tuple, x)
+    res = first(fs)(x)
+    return (res, extraChain(Base.tail(fs), res)...)
+end
+
+struct Dense{F,S,T}
+  W::S
+  b::T
+  σ::F
+end
+
+Dense(W, b) = Dense(W, b, identity)
+
+function Dense(in::Integer, out::Integer, σ = identity;
+               initW = glorot_uniform, initb = zeros)
+  return Dense(initW(out, in), initb(out), σ)
+end
+
+@functor Dense
+
+function (a::Dense)(x::AbstractArray)
+  W, b, σ = a.W, a.b, a.σ
+  σ.(W*x .+ b)
+end
+
+struct Diagonal{T}
+  α::T
+  β::T
+end
+
+Diagonal(in::Integer; initα = ones, initβ = zeros) =
+  Diagonal(initα(in), initβ(in))
+
+@functor Diagonal
+
+function (a::Diagonal)(x)
+  α, β = a.α, a.β
+  α.*x .+ β
+end
